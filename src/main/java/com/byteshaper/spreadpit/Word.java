216c77c3ba93978ba82841d4d1376719841e22ec
Henning Schuetz <henning@byteshaper.com>
2016-05-31 00:20:53 +0200

    still project setup

---
+++ src/main/java/com/byteshaper/spreadpit/Word.java
@@ -0,0 +1,66 @@
+package com.byteshaper.spreadpit;
+
+public class Word {
+	
+	private final int lineNumber;
+	
+	private String firstColumn;
+	
+	private String secondColumn;
+	
+	private String thirdColumn;
+	
+	public Word(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	public String getFirstColumn() {
+		return firstColumn;
+	}
+
+	public void setFirstColumn(String firstColumn) {
+		this.firstColumn = firstColumn;
+	}
+
+	public String getSecondColumn() {
+		return secondColumn;
+	}
+
+	public void setSecondColumn(String secondColumn) {
+		this.secondColumn = secondColumn;
+	}
+
+	public String getThirdColumn() {
+		return thirdColumn;
+	}
+
+	public void setThirdColumn(String thirdColumn) {
+		this.thirdColumn = thirdColumn;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + lineNumber;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Word other = (Word) obj;
+		if (lineNumber != other.lineNumber)
+			return false;
+		return true;
+	}
+}
