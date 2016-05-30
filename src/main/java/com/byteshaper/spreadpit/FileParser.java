0f80a86ca113aa42581805174d022a9e90d75acb
Henning Schuetz <henning@byteshaper.com>
2016-05-31 00:34:23 +0200

    work in progress

---
+++ src/main/java/com/byteshaper/spreadpit/FileParser.java
@@ -0,0 +1,14 @@
+package com.byteshaper.spreadpit;
+
+import org.odftoolkit.simple.SpreadsheetDocument;
+
+import java.util.List;
+
+
+public class FileParser {
+	
+	List<Word> readWords() {
+		SpreadsheetDocument.loadDocument(null); // continue
+		return null;
+	}
+}
